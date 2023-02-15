3c19331736abae772457392dde536e78f3cd0798
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-16 00:47:43 +0100

    Create jotaAsterisco.java
    Escribe un programa que imprima la letra J con asteriscos.

---
+++ Literales/jotaAsterisco.java
@@ -0,0 +1,24 @@
+// Escribe un programa que imprima la letra J con asteriscos.
+/*
+
+ ********
+       **
+       **
+       **
+ **    **
+ **    **
+  *******
+
+ */
+public class jotaAsterisco {
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println("      **");
+        System.out.println("      **");
+        System.out.println("      **");
+        System.out.println("**    **");
+        System.out.println("**    **");
+        System.out.println(" *******");
+
+    }
+}
