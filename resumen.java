6e8b3b302fde05c25645428d575b436737979566
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-17 00:29:41 +0100

    Create resumen.java

---
+++ resumen.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Locale;
+import java.util.Arrays;
+
+public class resumen {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // comentario
+    
+    /* comentario
+    en varias lineas*/
+    
+    int numeroEntero = 1;
+    float numeroDecimal = 1.5f;
+    boolean logica = true;
+    char unCaracter = 'a';
+    
+    String palabra = "Hola";
+    String frase = "Esto es una frase";
+    
+  }
+}
