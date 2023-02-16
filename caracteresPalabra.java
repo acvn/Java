f1d3f7b0bc853952f627046f7acf7adf248280e0
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-16 02:46:55 +0100

    Create caracteresPalabra.java
    Código para contar caracteres de una palabra.

---
+++ caracteresPalabra.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class caracteresPalabra {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // Pedimos al usuario escribir una palabra.
+    System.out.print("Escríbe una palabra: ");
+    // Usamos la clase Scanner para leer una entrada de texto del usuario y la guardamos en la variable "palabra".
+    String palabra = scanner.next();
+    // Utilizando el método "length" de la clase String podemos contar el número de caracteres de la palabra.
+    int totalCaracteres = palabra.length();
+    // Imprimimos el número total de caracteres.
+    System.out.println(palabra + " tiene " + totalCaracteres + " caracteres.");
+  }
+}
