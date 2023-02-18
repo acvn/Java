3d74b9f8cb0612e30991a36341f77f27e5aafdcb
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-18 04:55:44 +0100

    Create SumaSimple.java
    Distintas maneras de imprimir una suma por pantalla.

---
+++ Operadores/Aritméticos/SumaSimple.java
@@ -0,0 +1,27 @@
+public class SumaSimple {
+  public static void main(String[] args) {
+    // Declaramos la primera variable con nombre primerNumero y valor 5
+    int primerNumero = 5;
+    
+    // Declaramos la segunda variable con nombre segundoNumero y valor 4
+    int segundoNumero = 4;
+    
+    // Declaramos una tercera variable con nombre "suma" y valor primerNumero+segundoNumero
+    int primeraSuma = primerNumero+segundoNumero;
+    
+    // Imprimimos por terminal la variable primeraSuma
+    System.out.println(primeraSuma); // Imprime por pantalla 9
+    
+    // También podemos hacer la suma directamente sin usar las dos variables, solamente poniendo 5+4
+    int suma = 5+4; 
+    
+    // Imprimimos el valor de suma
+    System.out.println(suma); // Imprime por pantalla: 9
+    
+    // Otra manera de hacer imprimir la suma es poniendo los valores directamente en el propio println 
+    System.out.println(5+4); // Imprime por pantalla: 9
+    
+    // 
+
+  }
+}
