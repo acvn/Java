7c35816753b7f7d081b04ad7a7cafc5340dcd392
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-18 03:39:25 +0100

    Update and rename caracteresPalabra.java to CaracteresPalabra.java

--- CaracteresPalabra.java
+++ CaracteresPalabra.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class caracteresPalabra {
+public class CaracteresPalabra {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     // Pedimos al usuario escribir una palabra.
