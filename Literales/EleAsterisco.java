eeb6a668fecf8f88aaea1c845de38a2047542f77
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-20 21:05:27 +0100

    Create EleAsterisco.java
    Se ha añadido un ejercicio muy similar a JotaAsterisco.java pero con una letra diferente.

---
+++ Literales/EleAsterisco.java
@@ -0,0 +1,24 @@
+// Escribe un programa que imprima la letra L con asteriscos.
+/*
+ 
+ **
+ **
+ **
+ **
+ **
+ **
+ ********
+
+ */
+
+public class EleAsterisco {
+	public static void main(String[] args) {
+		System.out.println("**      ");
+		System.out.println("**      ");
+		System.out.println("**      ");
+		System.out.println("**      ");
+		System.out.println("**      ");
+		System.out.println("**      ");
+		System.out.println("********");
+	}
+}
