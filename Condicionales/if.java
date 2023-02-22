23b9427f80b6bb53e12139a3f886c0f3d3a7e349
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-22 01:43:09 +0100

    Create if.java
    Ejemplo del condicional IF

---
+++ Condicionales/if.java
@@ -0,0 +1,7 @@
+public class if {
+  public static void main(String[] args) {
+    float dinero = 3;
+    if (dinero>0) {
+      System.out.print("Tienes más de 0€ en la cuenta, exactamente: " + dinero);
+    }
+  }
